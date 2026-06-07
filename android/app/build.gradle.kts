dbe035ecd4de46924b900a9f66ce946ea61af0bf
Sudharsan4449 <sudharsan4449@gmail.com>
2026-06-07 20:09:50 +0530

    build: bump version to 1.0.21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.velstrack.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.0.20"
+        versionCode = 20
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
